e723d477b4ab4572b11470816cee0bdcd20c2806
onkar <onkar.chendage@gmail.com>
2024-02-10 14:13:08 +0530

    add: github repo clone to local

--- backend/upload-service/api/handler/handler.go
+++ backend/upload-service/api/handler/handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-git/go-git/v5"
 )
 
 func Home(c *gin.Context) {
@@ -12,11 +13,28 @@ func Home(c *gin.Context) {
 }
 
 func Upload(c *gin.Context) {
-	var requestData struct {
+	var body struct {
 		Url string `json:"q"`
 	}
 
-	if err := c.BindJSON(&requestData); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("%s url not found.", requestData.Url)})
+	if err := c.BindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("%s url not found.", body.Url)})
+		return
 	}
+
+	repo_id := "ndngdgn" // generate a unique id for the repo
+	file_path := fmt.Sprintf("/tmp/repo/%s", repo_id)
+
+	if _, err := git.PlainClone(file_path, false, &git.CloneOptions{
+		URL: body.Url,
+	}); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Error cloning the repository %s: %s", body.Url, err.Error())})
+		return
+	}
+
+	// upload to s3
+
+	// delete the repo from /tmp/repo
+
+	c.JSON(http.StatusOK, gin.H{"repo_id": repo_id, "message": "File uploaded successfully."})
 }
