8ad6dee285519eccc4615d21efeb4177b0a69432
onkar <onkar.chendage@gmail.com>
2024-06-08 14:33:44 +0530

    fix: s3 put presigned url

--- backend/repo-handler-service/services/urlPresignerServices.go
+++ backend/repo-handler-service/services/urlPresignerServices.go
@@ -1,37 +1,44 @@
 package services
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-func getPresignedURL(bucketName, objectKey string) (string, error) {
-	sess, err := session.NewSession(&aws.Config{
-		Region:                        aws.String(os.Getenv("AWS_REGION")),
-		CredentialsChainVerboseErrors: aws.Bool(true),
-		S3ForcePathStyle:              aws.Bool(true),
-		//SignatureVersion: aws.String("v4"),
-	})
+func putPresignURL(objectName string) (string, error) {
+	// Load the AWS configuration
+	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("unable to load AWS config: %w", err)
 	}
 
-	svc := s3.New(sess)
-	expiration := (10 * time.Minute)
-
-	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(objectKey),
-	})
-
-	url, err := req.Presign(expiration)
-	if err != nil {
-		return "", err
+	bucketName := os.Getenv("S3_BUCKET_NAME")
+	if bucketName == "" {
+		return "", fmt.Errorf("S3_BUCKET_NAME environment variable is not set")
 	}
 
-	return url, nil
+	// Create an S3 client
+	s3client := s3.NewFromConfig(cfg)
+
+	// Create a presign client
+	presignClient := s3.NewPresignClient(s3client)
+
+	// Generate a presigned URL
+	presignedUrl, err := presignClient.PresignPutObject(context.TODO(),
+		&s3.PutObjectInput{
+			Bucket: aws.String(bucketName),
+			Key:    aws.String(objectName),
+		},
+		s3.WithPresignExpires(15*time.Minute))
+	if err != nil {
+		return "", fmt.Errorf("unable to generate presigned URL: %w", err)
+	}
+
+	return presignedUrl.URL, nil
 }
