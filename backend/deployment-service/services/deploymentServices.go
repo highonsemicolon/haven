b058f07388f7f7305ade60bcd79205c9e27da563
onkar <onkar.chendage@gmail.com>
2024-07-02 00:47:20 +0530

    code cleanup

--- backend/deployment-service/services/deploymentServices.go
+++ backend/deployment-service/services/deploymentServices.go
@@ -3,12 +3,10 @@ package services
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	"os"
 
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
-	"github.com/onkarr19/haven/deployment-handler-service/models"
-	"github.com/onkarr19/haven/deployment-handler-service/repositories"
+	"github.com/onkarr19/haven/deployment-service/models"
+	"github.com/onkarr19/haven/deployment-service/repositories"
 	"github.com/pkg/errors"
 	"github.com/redis/go-redis/v9"
 )
@@ -33,72 +31,12 @@ func (s *deploymentService) CreateDeployment(deployment *models.Deployment) erro
 		return errors.Wrap(err, "error marshalling job to JSON")
 	}
 
-	if _, err := s.rds.RPush(context.Background(), "builder-input", job).Result(); err != nil {
+	inputQueue := os.Getenv("INOUT_QUEUE")
+
+	if _, err := s.rds.RPush(context.Background(), inputQueue, job).Result(); err != nil {
 		return errors.Wrap(err, "error pushing job to Redis")
 	}
 
-	if err := s.deploymentRepo.CreateDeployment(deployment); err != nil {
-		return errors.Wrap(err, "error saving deployment to repository")
-	}
-	return nil
-}
-
-func (s *deploymentService) CreateDeploymentbackup(deployment *models.Deployment) error {
-
-	// Generate a presigned URL
-	presignedURL, err := putPresignURL(deployment.Name)
-	if err != nil {
-		return errors.Wrap(err, "error generating presigned URL")
-	}
-
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		return errors.Wrap(err, "error initializing Docker client")
-	}
-	defer cli.Close()
-
-	ctx := context.Background()
-
-	config := &container.Config{
-		Image:      "nodewithgit",
-		WorkingDir: "/app",
-		Cmd: []string{
-			"sh", "-c",
-			fmt.Sprintf(`git clone -b "%s" "%s" . && npm install && npm run build && zip -r build-artifacts.zip build/* &&
-		            curl --upload-file build-artifacts.zip "%s"`, deployment.Branch, deployment.GitURL, presignedURL),
-		},
-	}
-
-	// Create a container
-	resp, err := cli.ContainerCreate(ctx, config, nil, nil, nil, "")
-	if err != nil {
-		return errors.Wrap(err, "error creating Docker container")
-	}
-	defer func() {
-		if err := cli.ContainerRemove(ctx, resp.ID, container.RemoveOptions{Force: true}); err != nil {
-			errors.Wrap(err, "error removing Docker container")
-		}
-	}()
-
-	// Start the container
-	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
-		return errors.Wrap(err, "error starting Docker container")
-	}
-
-	// Wait for the container to finish
-	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-	select {
-	case err := <-errCh:
-		if err != nil {
-			return errors.Wrap(err, "error waiting for Docker container to finish")
-		}
-	case <-statusCh:
-	}
-
-	// set the hosted URL for deployment
-	deployment.HostedURL = "https://" + deployment.Name + ".haven.app"
-
-	// Save the deployment to the repository
 	if err := s.deploymentRepo.CreateDeployment(deployment); err != nil {
 		return errors.Wrap(err, "error saving deployment to repository")
 	}
